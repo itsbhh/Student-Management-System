d6397fe9a5d4495127db904bb1686eddb4178459
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2025-11-30 12:03:11 +0530

    Create Teacher.java

---
+++ src/main/java/org/example/Teacher.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Teacher {
+    public String id;
+    public String name;
+    public String password;
+
+    public Teacher() {}
+
+    public Teacher(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+}
